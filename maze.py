9d820c64c85b86bf0d223084583b2d7fdcb687ef
toyeafo <afolabiolatoye@gmail.com>
2024-08-03 15:00:39 +0100

    Maze class updated

--- maze.py
+++ maze.py
@@ -21,18 +21,6 @@ class Maze:
     def _draw_cell(self, I, J):
         if self._win is None:
             return
-        # current_x1 = self._x1
-        # current_x2 = self._cell_size_x
-        # current_y1 = self._y1
-        # current_y2 = self._cell_size_y
-
-        # if I != 0:
-        #     current_y1 = current_y2
-        #     current_y2 = current_y1 * I
-            
-        # if J != 0:
-        #     current_x1 = current_x2
-        #     current_x2 = current_x1 * J
 
         current_x1 = self._x1 + I * self._cell_size_x
         current_y1 = self._y1 + J * self._cell_size_y
@@ -43,5 +31,4 @@ class Maze:
         self._animate()
 
     def _animate(self):
-        # self._win.redraw()
         self._win.root.after(1000, self._win.redraw())
